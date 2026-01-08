c91cb0c783100ac93e5e988e89d4f5e161e21132
boyapati506 <balaji.boyapati506@gmail.com>
2026-01-08 11:46:43 +0530

    made changes

---
+++ bootstrap/main.tf
@@ -0,0 +1,30 @@
+resource "aws_s3_bucket" "gitops_tf_state"{
+    bucket = "gitops_terraform_state"
+    force_destroy = false
+}
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.gitops_tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.gitops_tf_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "tf_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
