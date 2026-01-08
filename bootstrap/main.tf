24cb12453cc6477b42c1c6e89a2b70e565261a75
boyapati506 <balaji.boyapati506@gmail.com>
2026-01-08 11:55:23 +0530

    made changes

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -18,7 +18,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
   }
 }
 
-resource "aws_dynamodb_table" "tf_locks" {
+resource "aws_dynamodb_table" "gitops_tf_locks" {
   name         = "terraform-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
